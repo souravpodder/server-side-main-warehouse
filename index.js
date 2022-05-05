8a9f57998737254e3f7f85e31edda5fc0046376a
spodder <spodder37@gmail.com>
2022-05-06 00:21:16 +0600

    getting items api added

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-console.log('hello warehouse');
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6bnyi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+  try {
+    await client.connect();
+
+    const itemsCollection = client.db("sportsGearDB").collection("items");
+    console.log('connected to sports gear db');
+
+    // get all the datas
+    app.get('/items', async (req, res) => {
+      const query = req.query;
+      // console.log(query);
+
+      const cursor = itemsCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+  } finally {
+
+  }
+}
+
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+  res.send('inverntory server is running....');
+})
+
+app.listen(port, () => {
+  console.log('listening to port', port);
+})
