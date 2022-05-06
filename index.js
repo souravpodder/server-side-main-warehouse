a18edbf66ade976f015c8214289aa106406948b1
spodder <spodder37@gmail.com>
2022-05-06 17:25:16 +0600

    quantity update api added

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(cors());
 app.use(express.json());
@@ -28,6 +28,36 @@ async function run() {
       const result = await cursor.toArray();
       res.send(result);
     })
+
+    // get a single item 
+    app.get('/item/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const item = await itemsCollection.findOne(query);
+      // console.log(item);
+      res.send(item);
+    })
+
+    //update quantity 
+    app.put(`/updatequantity/:id`, async (req, res) => {
+      const id = req.params.id;
+      const updatedQuantity = req.body;
+      console.log(updatedQuantity);
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
+
+      const updateDoc = {
+        $set: {
+          quantity: updatedQuantity.quantity
+        }
+
+      }
+
+      const result = await itemsCollection.updateOne(filter, updateDoc, options);
+      res.send(result);
+    }
+    )
+
 
   } finally {
 
